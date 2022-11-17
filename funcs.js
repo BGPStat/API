755732199662a4ab5d8677fcd3ec445ce904305a
SamCoThePug <46136177+SamCoThePug@users.noreply.github.com>
2022-11-17 19:36:44 +0000

    Create funcs.js

---
+++ funcs.js
@@ -0,0 +1,14 @@
+function execute(command, callback){
+    exec(command, function(error, stdout, stderr){ callback(stdout); });
+};
+function setIntervalImmediately(func, interval) {
+  func();
+  return setInterval(func, interval);
+}
+let v6cmd = "birdc s r table master6 | grep 'AS' | grep '::' | grep '/' | wc -l"
+let v4cmd = "birdc s r table master4 | grep 'AS' | grep '/' | wc -l"
+let v6asnscmd = `birdc s r table master6 | grep 'AS' | grep '/' | grep -oE "[^ ]+$" | sed "s/i//g" | sed "s/e//g" | sed 's/[^a-zA-Z0-9]//g'`
+let v4asnscmd = `birdc s r table master4 | grep 'AS' | grep '/' | grep -oE "[^ ]+$" | sed "s/i//g" | sed "s/e//g" | sed 's/[^a-zA-Z0-9]//g'`
+let asroutes = `birdc s r where bgp_path.last = {{ASN}} | rg -j 16 'AS' | rg -j 16 'unicast' | rg -j 16 '/' | cut -d ' ' -f1`
+
+module.exports = {execute:execute,setIntervalImmediately:setIntervalImmediately,v6cmd:v6cmd,v4cmd:v4cmd,v6asnscmd:v6asnscmd,v4asnscmd:v4asnscmd,asroutes:asroutes}
